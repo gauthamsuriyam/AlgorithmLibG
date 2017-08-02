9fa690b904fbb7c467a6a164d7b39390c627ab72
Sharmili Sitharthan <Sharmili@Sharmilis-MacBook-Air.local>
2017-08-02 15:20:04 -0600

    New project for neural nets created.

---
+++ NeuralNets/NeuralNetwork/NeuralNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNetwork
+//
+//  Created by Sharmili Sitharthan on 8/2/17.
+//  Copyright © 2017 GauthamSuriya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
