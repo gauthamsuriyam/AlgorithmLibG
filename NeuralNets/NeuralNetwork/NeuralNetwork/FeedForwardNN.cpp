8b5e27a03106725d7fa0d209810311cacfd4b736
Gautham Suriya <gauthamsuriya@praveens-macbook-pro.local>
2017-08-17 22:06:46 -0700

    Directory explorer

--- NeuralNets/NeuralNetwork/NeuralNetwork/FeedForwardNN.cpp
+++ NeuralNets/NeuralNetwork/NeuralNetwork/FeedForwardNN.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <dirent.h>
 
 using namespace std;
 
@@ -32,13 +33,24 @@ void FeedForwardNN::BackProp(){
 
 void FeedForwardNN::TextProcessor(){
     string line;
-    ifstream testfile("/Users/Sharmili/Desktop/GauthamDev/20news-bydate/20news-bydate-train/talk.religion.misc/83476");
-    if(testfile.is_open()){
+    
+    //ifstream testfile("/Users/gauthamsuriya/Desktop/GauthamDev/AlgorithmLibG/20news-bydate/20news-bydate-train/talk.religion.misc/83476");
+    DIR *dirp = opendir("/Users/gauthamsuriya/Desktop/GauthamDev/AlgorithmLibG/20news-bydate/");
+    struct dirent * rePt;
+    if(dirp){
+        while((rePt = readdir(dirp)) != NULL){
+            if( strcmp(rePt->d_name, ".") != 0 && strcmp(rePt->d_name, "..") != 0 && strcmp(rePt -> d_name, ".DS_Store"))
+                cout << rePt->d_name << "\n";
+        }
+        closedir(dirp);
+    }
+    
+    /*if(testfile.is_open()){
         while(getline(testfile, line)){
             cout<< line << "\n" << endl;
         }
         testfile.close();
     }else{
         cout << "unable to open file" << endl;
-    }
+    }*/
 }
